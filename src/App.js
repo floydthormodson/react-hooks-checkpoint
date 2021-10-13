59893154bbd083367eada48a499ce61d3cb1d311
floydthormodson <floydthormodson@outlook.com>
2021-10-13 15:18:28 -0400

    The images in the product cards themselves display one at a time. I have added a blank component to incorporate to simplify what elements display.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import './App.css';
-import {Card,CardContent,CardHeader,CardMedia,} from '@material-ui/core';
+import {Card,CardContent,CardMedia,} from '@material-ui/core';
 
 
 function App() {
@@ -12,11 +12,9 @@ function App() {
   const [currentCategory, setCurrentCategory] =useState('');
   const [currentPrice, setCurrentPrice]=useState('');
   const [currentImage, setCurrentImage] =useState('')
+
   
   
-
-
-
   useEffect(() =>{
     async function callProductList(){
       let res =await fetch("http://52.26.193.201:3000/products/list");
@@ -24,7 +22,7 @@ function App() {
       setProductList(json)
     };
     callProductList();
-  }, [currentProduct]);
+  }, []);
 
   useEffect(() =>{
     async function callProductID(){
@@ -48,60 +46,60 @@ function App() {
     callImage();
   }, [currentProduct]);
 
-
-  console.log(currentImage)
   return (
     <div className="App">
       <div className="app-title flex-column">
         <h1>React Hooks Checkpoint</h1>
       </div>
       <div className="Card-Section">
-      <Card class="card" id="main-display">
-      You are currently Viewing:<br /> 
-      {currentName}<br />
-      {currentSlogan}<br />
-      {currentCategory}<br />
-      {currentPrice}<br />
-      <img src={currentImage}/>
-      </Card>
-      <Card className="card" id="0">
+        <Card class="card" id="main-display">
+          You are currently Viewing:<br /> 
+          {currentName}<br />
+          {currentSlogan}<br />
+          {currentCategory}<br />
+          {currentPrice}<br />
+          <img src={currentImage}/>
+        </Card>
+        <Card className="card" id="0">
           <CardMedia />
           <CardContent className="card">
             {productList[0]?.name}<br />
+            <img src={currentProduct === 1? currentImage: ''}/>
             <button onClick={()=> setCurrentProduct(1)}>See More</button>
           </CardContent>
-          
-      </Card>
-      <Card className="card" id="1">
-          <CardMedia />
-          <CardContent className="card">
-            {productList[1]?.name}<br />
-            <button onClick={(e)=> setCurrentProduct(2)}>See More</button>
-          </CardContent>
-          
-      </Card>
-      <Card className="card" id="2">
-          <CardMedia src={currentImage}/>
-          <CardContent className="card">
-            {productList[2]?.name}<br />
-            <button onClick={(e)=> setCurrentProduct(3)}>See More</button>
-          </CardContent>
-      </Card>
-      <Card className="card" id="3">
-          <CardMedia />
-          <CardContent className="card">
-            {productList[3]?.name}<br />
-            <button onClick={(e)=> setCurrentProduct(4)}>See More</button>
-          </CardContent><br/>
-          
-      </Card>
-      <Card className="card" id="4">
-          <CardMedia />
-          <CardContent className="card">
-            {productList[4]?.name}<br />
-            <button onClick={(e)=> setCurrentProduct(5)}>See More</button>
-          </CardContent>
-      </Card>
+        </Card>
+        <Card className="card" id="1">
+            <CardMedia />
+            <CardContent className="card">
+              {productList[1]?.name}<br />
+              <img src={currentProduct === 2? currentImage: ''}/>
+              <button onClick={(e)=> setCurrentProduct(2)}>See More</button>
+            </CardContent>
+        </Card>
+        <Card className="card" id="2">
+            <CardMedia src={currentImage}/>
+            <CardContent className="card">
+              {productList[2]?.name}<br />
+              <img src={currentProduct === 3? currentImage: ''}/>
+              <button onClick={(e)=> setCurrentProduct(3)}>See More</button>
+            </CardContent>
+        </Card>
+        <Card className="card" id="3">
+            <CardMedia />
+            <CardContent className="card">
+              {productList[3]?.name}<br />
+              <img src={currentProduct === 4? currentImage: ''}/>
+              <button onClick={(e)=> setCurrentProduct(4)}>See More</button>
+            </CardContent><br/>
+        </Card>
+        <Card className="card" id="4">
+            <CardMedia />
+            <CardContent className="card">
+              {productList[4]?.name}<br />
+              <img src={currentProduct === 5? currentImage: ''}/>
+              <button onClick={(e)=> setCurrentProduct(5)}>See More</button>
+            </CardContent>
+        </Card>
       </div>
     </div>
   );
