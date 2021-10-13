11ad1d2c26c2fcea41b1d21f53d06c0c8de02a9a
floydthormodson <floydthormodson@outlook.com>
2021-10-13 18:23:19 -0400

    Added an alt prop to the img lines at lines 62, 72, 82, 92, and 102 to clear the warnings in the console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import './App.css';
-import {Card,CardContent,CardMedia,} from '@material-ui/core';
+import {Card,CardContent} from '@material-ui/core';
 import DisplayContent from "./DisplayContent.js";
 
 
@@ -13,7 +13,6 @@ function App() {
   const [currentCategory, setCurrentCategory] =useState('');
   const [currentPrice, setCurrentPrice]=useState('');
   const [currentImage, setCurrentImage] =useState('');
-  const [toggle, setToggle] =useState(false);
 
   
   useEffect(() =>{
@@ -55,57 +54,52 @@ function App() {
       </div>
       <div className="Card-Section">
         <Card className="card" id="0">
-          <CardMedia />
           <CardContent className="card">
             {productList[0]?.name}<br />
             <DisplayContent currentProduct={currentProduct} selector = {1}
               name={currentName} slogan={currentSlogan} category={currentCategory} price={currentPrice}
             />
-            <img src={currentProduct === 1? currentImage: ''}/>
+            <img src={currentProduct === 1? currentImage: ''} alt=""/>
             <button onClick={()=> setCurrentProduct(1)}>See More</button><br />
           </CardContent>
         </Card>
         <Card className="card" id="1">
-            <CardMedia />
             <CardContent className="card">
               {productList[1]?.name}<br />
               <DisplayContent currentProduct={currentProduct} selector = {2}
                 name={currentName} slogan={currentSlogan} category={currentCategory} price={currentPrice}
               />
-              <img src={currentProduct === 2? currentImage: ''}/>
+              <img src={currentProduct === 2? currentImage: ''} alt=""/>
               <button onClick={(e)=> setCurrentProduct(2)}>See More</button>
             </CardContent>
         </Card>
         <Card className="card" id="2">
-            <CardMedia src={currentImage}/>
             <CardContent className="card">
               {productList[2]?.name}<br />
               <DisplayContent currentProduct={currentProduct} selector = {3}
                 name={currentName} slogan={currentSlogan} category={currentCategory} price={currentPrice}
               />
-              <img src={currentProduct === 3? currentImage: ''}/>
+              <img src={currentProduct === 3? currentImage: ''} alt=""/>
               <button onClick={(e)=> setCurrentProduct(3)}>See More</button>
             </CardContent>
         </Card>
-        <Card className="card" id="3">
-            <CardMedia />
-            <CardContent className="card">
+        <Card className="card" id="3" >
+            <CardContent className="card" >
               {productList[3]?.name}<br />
               <DisplayContent currentProduct={currentProduct} selector = {4}
                 name={currentName} slogan={currentSlogan} category={currentCategory} price={currentPrice}
               />
-              <img src={currentProduct === 4? currentImage: ''}/>
+              <img src={currentProduct === 4? currentImage: ''} alt=""/>
               <button onClick={(e)=> setCurrentProduct(4)}>See More</button>
             </CardContent><br/>
         </Card>
         <Card className="card" id="4">
-            <CardMedia />
             <CardContent className="card">
               {productList[4]?.name}<br />
               <DisplayContent currentProduct={currentProduct} selector = {5}
                 name={currentName} slogan={currentSlogan} category={currentCategory} price={currentPrice}
               />
-              <img src={currentProduct === 5? currentImage: ''}/>
+              <img src={currentProduct === 5? currentImage: ''} alt=""/>
               <button onClick={(e)=> setCurrentProduct(5)}>See More</button>
             </CardContent>
         </Card>
